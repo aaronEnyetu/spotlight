770618bb7f983e6af9c7fc68650d7758569f5cba
Aaron Enyetu <aaronenyetu@gmail.com>
2022-05-03 11:12:02 -0700

    Add Exclamation Points Function

--- test/functions.test.js
+++ test/functions.test.js
@@ -1,14 +1,33 @@
 // IMPORT MODULES under test here:
-import { myFunction } from '../functions.js';
+import { addExclamationPoints, myFunction } from '../functions.js';
 
 const { test, skip } = QUnit;
 
 test('this test should pass', (expect) => {
-    const expected = true;
+    const expected = 'bunny rabbit!!!';
 
-    const actual = myFunction();
+    const actual = myFunction('bunny rabbit');
 
-    expect.equal(actual, expected, 'true = true');
+    expect.equal(actual, expected, 'bunny rabbit');
+
+    const expected2 = 'cat!!!';
+
+    const actual2 = addExclamationPoints('cat');
+    expect.equal(actual2, expected2, 'cat');
+
+    const expected3 = 'dog!!!';
+
+    const actual3 = addExclamationPoints('dog');
+    expect.equal(actual3, expected3, 'dog');
+
+    const expected4 = 'go sloth!!!';
+
+    const actual4 = addExclamationPoints('go sloth');
+    expect.equal(actual4, expected4, 'go sloth');
+
+    console.log(actual4);
+
+
 });
 
 skip('this test should be skipped', (expect) => {
